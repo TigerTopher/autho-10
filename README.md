23d20da3256bb8f6592f21096bda9f0873f05eb6
Christopher Vizcarra <9291322+TigerTopher@users.noreply.github.com>
2020-07-18 01:24:10 +1000

    Throw in more stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # autho-10
+
+This adopts Randall Deggess' auth prezy to setup a simple app. Add a few adjustments (Typescript ftw).
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,41 @@
 import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 const app = express();
-const port = 8080; // default port to listen
+const PORT = 3000;
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.set('views', path.join(__dirname, '../src/views'));
+app.set("view engine", "pug");
 
-// start the Express server
-app.listen( port, () => {
+mongoose.connect("mongodb://localhost/ss-auth");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+})
+
+app.post("/register", (req, res) => {
+    res.json(req.body);
+})
+
+app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
-
-// import express from "express";
-
-// const app = express();
-
-// app.set("view engine", "pug");
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// })
-
-// app.get("/register", (req, res) => {
-//   res.render("register");
-// })
-
-// app.get("/login", (req, res) => {
-//   res.render("login");
-// })
-
-// app.get("/dashboard", (req, res) => {
-//   res.render("dashboard");
-// })
-
-// app.listen(3000);
+    console.log(`server started at http://localhost:${PORT}`);
+});
